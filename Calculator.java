4eca02a4a2573121441fbc61d3d3f6410ace4ac4
Ankit_kumar <ankit95001kumar@gmail.com>
2025-01-07 12:03:13 +0530

    Add function

--- Calculator.java
+++ Calculator.java
@@ -4,23 +4,13 @@ public class Calculator {
     public static int add(int num1,int num2){
 	return num1+num2;
     }
-    public static int subtraction(int num1,int num2){
-	return num1-num2;
-    }
-    public static int multiplication(int num1,int num2){
-	return num1*num2;
-    }
-    public static int division(int num1,int num2){
-	return num1/num2;
-    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("Select an operation:");
         System.out.println("1. Addition");
-	System.out.println("2. Subtraction");
-	System.out.println("3. Multiplication");
-        System.out.println("4. Division");
+
 	System.out.print("Enter your choice (1-4): ");
         
         int choice = scanner.nextInt();
@@ -33,19 +23,6 @@ public class Calculator {
         switch (choice) {
             case 1: // Addition
                 System.out.println("Result: " + add(num1,num2));
-                break;
-	    case 2: // Subtraction
-                System.out.println("Result: " + subtraction(num1,num2));
-                break;
-	    case 3: // Multiplication
-                System.out.println("Result: " + multiplication(num1,num2));
-                break;
-            case 4: // Division
-                if (num2 != 0) {
-                    System.out.println("Result: " + division(num1,num2));
-                } else {
-                    System.out.println("Error: Division by zero is not allowed.");
-                }
                 break;
             default:
                 System.out.println("Invalid choice! Please select a valid operation.");
