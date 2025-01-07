8068e8a4082bc56f6a0099592e2419b4f0331af9
Ankit_kumar <ankit95001kumar@gmail.com>
2025-01-07 12:04:42 +0530

    Calculator is created

--- Calculator.java
+++ Calculator.java
@@ -1,18 +1,7 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static int add(int num1,int num2){
-	return num1+num2;
-    }
-    public static int subtraction(int num1,int num2){
-	return num1-num2;
-    }
-    public static int multiplication(int num1,int num2){
-	return num1*num2;
-    }
-    public static int division(int num1,int num2){
-	return num1/num2;
-    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
@@ -30,27 +19,7 @@ public class Calculator {
         System.out.print("Enter the second number: ");
         int num2 = scanner.nextInt();
         
-        switch (choice) {
-            case 1: // Addition
-                System.out.println("Result: " + add(num1,num2));
-                break;
-	    case 2: // Subtraction
-                System.out.println("Result: " + subtraction(num1,num2));
-                break;
-	    case 3: // Multiplication
-                System.out.println("Result: " + multiplication(num1,num2));
-                break;
-            case 4: // Division
-                if (num2 != 0) {
-                    System.out.println("Result: " + division(num1,num2));
-                } else {
-                    System.out.println("Error: Division by zero is not allowed.");
-                }
-                break;
-            default:
-                System.out.println("Invalid choice! Please select a valid operation.");
-        }
-        
+                
         scanner.close();
     }
 }
