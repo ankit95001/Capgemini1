c7c7fde27522af5843c7d49581c233421c8cc600
Ankit_kumar <ankit95001kumar@gmail.com>
2025-01-07 12:09:25 +0530

    subtraction

--- Calculator.java
+++ Calculator.java
@@ -4,12 +4,16 @@ public class Calculator {
     public static int add(int num1,int num2){
 	return num1+num2;
     }
+    public static int subtraction(int num1,int num2){
+	return num1-num2;
+    }
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         System.out.println("Select an operation:");
         System.out.println("1. Addition");
+	System.out.println("2. Subtraction");
 
 	System.out.print("Enter your choice (1-4): ");
         
@@ -23,6 +27,9 @@ public class Calculator {
         switch (choice) {
             case 1: // Addition
                 System.out.println("Result: " + add(num1,num2));
+                break;
+	    case 2: // Subtraction
+                System.out.println("Result: " + subtraction(num1,num2));
                 break;
             default:
                 System.out.println("Invalid choice! Please select a valid operation.");
